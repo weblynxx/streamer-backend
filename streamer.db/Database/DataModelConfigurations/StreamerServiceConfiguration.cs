cd38f74e798798b9cda1b0521f269e9b3af811ce
weblynxx <62029041+weblynxx@users.noreply.github.com>
2021-11-08 23:17:27 +0300

    add tables Service, StreamerService

---
+++ streamer.db/Database/DataModelConfigurations/StreamerServiceConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using streamer.db.Database.DataModel;
+
+namespace streamer.db.Database.DataModelConfigurations
+{
+    public class StreamerServiceConfiguration: IEntityTypeConfiguration<StreamerServiceDm>
+    {
+        public void Configure(EntityTypeBuilder<StreamerServiceDm> builder)
+        {
+            builder.ToTable("StreamerService");
+            builder.HasKey(x => x.Id);
+
+            builder.HasOne(pt => pt.Streamer)
+                .WithMany(p => p.StreamerServices)
+                .HasForeignKey(pt => pt.StreamerId);
+
+            builder.HasOne(pt => pt.Service)
+                .WithMany(p => p.StreamerServices)
+                .HasForeignKey(pt => pt.ServiceId);
+        }
+    }
+}
