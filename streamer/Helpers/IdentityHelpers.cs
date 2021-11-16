360c11ebda421fe2d45534b8cb452da8b323d055
weblynxx <62029041+weblynxx@users.noreply.github.com>
2021-11-16 21:51:36 +0300

    delete unused

--- streamer/Helpers/IdentityHelpers.cs
+++ streamer/Helpers/IdentityHelpers.cs
@@ -10,19 +10,6 @@ namespace streamer.Helpers
     public static class IdentityExtensions
     {
         /// <summary>
-        /// Get CompanyId
-        /// </summary>
-        /// <param name="principal"></param>
-        /// <returns></returns>
-        public static Guid GetLoggedUserCompanyId(this ClaimsPrincipal principal)
-        {
-            var claim = principal?.FindFirst(JwtConstants.IdCompanyClaim);
-
-            return claim == null ? Guid.Empty : Guid.Parse(claim.Value);
-        }
-
-
-        /// <summary>
         /// Get UserId
         /// </summary>
         /// <param name="principal"></param>
@@ -32,14 +19,6 @@ namespace streamer.Helpers
             var claim = principal?.FindFirst(JwtConstants.IdClaim);
 
             return claim == null ? Guid.Empty : Guid.Parse(claim.Value);
-        }
-
-
-        public static bool IsImpersonating(this ClaimsPrincipal principal)
-        {
-            var claim = principal?.FindFirst(JwtConstants.IsImpersonating);
-
-            return claim == null ? false : true;
         }
 
 
