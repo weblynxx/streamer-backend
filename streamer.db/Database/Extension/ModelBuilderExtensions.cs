848604d01c648dd1bbc91b18d7908b0b930dc07b
weblynxx <62029041+weblynxx@users.noreply.github.com>
2021-12-02 12:23:32 +0300

    add admin init

--- streamer.db/Database/Extension/ModelBuilderExtensions.cs
+++ streamer.db/Database/Extension/ModelBuilderExtensions.cs
@@ -22,7 +22,20 @@ namespace streamer.db.Database.Extension
                     Id = new Guid("83655a7a-92e0-4ce0-9d0c-f36fd5692a2a"),
                     Name = "Youtube"
                 }
-            );
+           );
+
+           modelBuilder.Entity<StreamerDm>().HasData(
+               new StreamerDm
+               {
+                   Id = new Guid("cf111dde-367b-4528-8558-3d753371ce34"),
+                   SecurityStamp = "09355be4-a18d-41a9-acce-dd48c68211d7",
+                   UserName = "admin",
+                   NormalizedUserName = "admin".ToUpper(),
+                   FirstName = "John",
+                   LastName = "Admin",
+                   PasswordHash = Hasher.HashPassword(null, "fuckThisSheet_DF$%123!@#"),
+                   Authorities = "ROLE_ADMIN",
+               });
 
         }
     }
---
+++ streamer.db/Migrations/20211202084935_AddInit-Admin.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace streamer.db.Migrations
+{
+    public partial class AddInitAdmin : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Streamer",
+                columns: new[] { "Id", "AccessFailedCount", "Authorities", "City", "ConcurrencyStamp", "CreatedDate", "Email", "EmailConfirmed", "Entrance", "FirstName", "Flat", "Floor", "House", "HouseBuilding", "IntercomCode", "LastLoginDate", "LastName", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "Password", "PasswordHash", "Phone", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "StreamerId", "Street", "Token", "TwoFactorEnabled", "UserName" },
+                values: new object[] { new Guid("cf111dde-367b-4528-8558-3d753371ce34"), 0, "ROLE_ADMIN", null, "e5a21b03-ea96-4cf8-8d02-13ee673c7d9d", new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), null, false, null, "John", null, 0, null, null, null, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Admin", false, null, null, "ADMIN", null, "AQAAAAEAACcQAAAAEM7S9LsgsHiJ95uVMfsE6Gc5ADJk7/WWD3SBGlL7tz2MP4gE/StdBK5+O6gXcAkj8w==", null, null, false, "09355be4-a18d-41a9-acce-dd48c68211d7", new Guid("00000000-0000-0000-0000-000000000000"), null, null, false, "admin" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Streamer",
+                keyColumn: "Id",
+                keyValue: new Guid("cf111dde-367b-4528-8558-3d753371ce34"));
+        }
+    }
+}
